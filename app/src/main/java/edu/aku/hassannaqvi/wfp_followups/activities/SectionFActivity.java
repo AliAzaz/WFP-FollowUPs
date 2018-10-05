0b9d475abee3b1f9067db781db01dfe52034a204
mustafa.ansari <mustufa_ayub@hotmail.com>
2018-10-05 17:30:18 +0500

    Section D added

---
+++ app/src/main/java/edu/aku/hassannaqvi/wfp_followups/activities/SectionFActivity.java
@@ -0,0 +1,17 @@
+package edu.aku.hassannaqvi.wfp_followups.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.aku.hassannaqvi.wfp_followups.R;
+
+public class SectionFActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_f);
+    }
+}
