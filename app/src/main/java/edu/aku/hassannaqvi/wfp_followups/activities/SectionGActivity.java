c88c05e9560adb43b71f59d7ad68b6d04b4637c3
mustafa.ansari <mustufa_ayub@hotmail.com>
2018-10-09 10:54:11 +0500

    commit

--- app/src/main/java/edu/aku/hassannaqvi/wfp_followups/activities/SectionGActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/wfp_followups/activities/SectionGActivity.java
@@ -3,6 +3,8 @@ package edu.aku.hassannaqvi.wfp_followups.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import edu.aku.hassannaqvi.wfp_followups.R;
+
 public class SectionGActivity extends AppCompatActivity {
 
     @Override
