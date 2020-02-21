486f297ced767f56e6eb4b500c02e94912a242ef
ali.azaz <ali.azaz.alam@hotmail.com>
2020-02-21 18:20:02 +0500

    done changes as stated by Mr.Amjad

--- app/src/main/java/edu/aku/hassannaqvi/wfp_followups/activities/SectionBActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/wfp_followups/activities/SectionBActivity.java
@@ -99,34 +99,30 @@ public class SectionBActivity extends AppCompatActivity {
                     ClearClass.ClearAllCardFields(bi.fldgrppfbb, true);
                 }
 
+                bi.fldgrppfb04.setVisibility(View.GONE);
+                bi.fldgrppfb05.setVisibility(View.GONE);
+                bi.fldgrppfb06.setVisibility(View.GONE);
+                bi.pfb04.setText(null);
+                bi.pfb05.setText(null);
+                bi.pfb06.setText(null);
+
                 if (i == bi.pfb02e.getId()) {
                     if (AppMain.currentPrg) {
                         bi.fldgrppfb04.setVisibility(View.VISIBLE);
                         bi.fldgrppfb05.setVisibility(View.GONE);
-                        return;
-                    } else {
-                        bi.fldgrppfb04.setVisibility(View.GONE);
                     }
                 }
 
                 if (i == bi.pfb02d.getId() || i == bi.pfb02h.getId()) {
                     bi.fldgrppfb04.setVisibility(View.VISIBLE);
                     bi.fldgrppfb05.setVisibility(View.GONE);
-                    return;
-                } else {
-                    bi.fldgrppfb04.setVisibility(View.GONE);
+                    bi.fldgrppfb06.setVisibility(View.GONE);
                 }
 
                 if (i == bi.pfb02f.getId() || i == bi.pfb02g.getId()) {
                     bi.fldgrppfb06.setVisibility(View.VISIBLE);
                     bi.fldgrppfb04.setVisibility(View.VISIBLE);
                     bi.fldgrppfb05.setVisibility(View.VISIBLE);
-                } else {
-                    bi.fldgrppfb06.setVisibility(View.GONE);
-                    bi.fldgrppfb04.setVisibility(View.GONE);
-                    bi.fldgrppfb05.setVisibility(View.GONE);
-                    bi.pfb06.setText(null);
-                    bi.pfb05.setText(null);
                 }
             }
         });
